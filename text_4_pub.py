5a0075527e03269c6563e03849292a7c744b209a
artprosvetov <prosvetov@gmail.com>
2016-02-21 15:12:21 +0300

    stem included

--- text_4_pub.py
+++ text_4_pub.py
@@ -20,12 +20,14 @@ import sklearn.feature_extraction
 import sklearn.svm
 from sklearn.externals import joblib
 import codecs
+import nltk.stem
 
 class Site_4_publication:
     def __init__(self, clf_file=None):
         self.y = []
         self.text = []
-        self.X_TV = sklearn.feature_extraction.text.TfidfVectorizer()            
+        self.X_TV = sklearn.feature_extraction.text.TfidfVectorizer()
+        self.text_stammer = nltk.stem.SnowballStemmer('russian')           
         self.text_load()
         if not clf_file:
             self.classification_train()
@@ -40,36 +42,27 @@ class Site_4_publication:
     def text_open(self, text_file, Geek_flag):
         text = pn.read_json(text_file)
         text['text'] = text['text'].replace('[^a-zA-Z0-9]', ' ', regex = True)
+        
         for i in text['text']:
-            #self.text = map(lambda x: x.lower(), self.text)
-            self.text.append(i.lower())
+            stem = " ".join([self.text_stammer.stem(word.lower()) for word in i.split()])
+            self.text.append(stem)
         if Geek_flag == 1:
             self.y = self.y + [0]*len(text)
         else:
             self.y = self.y + [1]*len(text)
         
-    def text_explore(self):
-        
-            
-        
-        
+    def text_explore(self): 
         self.X_dat = self.X_TV.fit_transform(self.text)
         
         
     def classification_train(self):
         X_train, X_test, y_train, y_test = sklearn.cross_validation.train_test_split(self.X_dat,\
-                                      self.y, test_size=0.2, random_state=142)    
-        self.clf = sklearn.svm.LinearSVC(C = 1.0, penalty='l2')
-        self.my_svm = self.clf.fit(X_train, y_train)
+                                      self.y, test_size=0.2, random_state=246)    
+        clf = sklearn.svm.LinearSVC(C = 1.0, penalty='l2')
+        self.my_svm = clf.fit(X_train, y_train)        
         self.plot_rocauc(y_test, X_test)
         
-        
-        
-    def classification_test(self, y_test= [], X_test= []):
-        X_train, X_test, y_train, y_test = sklearn.cross_validation.train_test_split(self.X_dat, \
-                                      self.y, test_size=0.1, random_state=684)
-        return sklearn.metrics.roc_auc_score(y_test, self.my_svm.predict(X_test))      
-        
+                       
     def save(self, file_name):
         joblib.dump(self.my_svm, file_name)
         print 'Save complete!'
@@ -90,14 +83,11 @@ class Site_4_publication:
         plt.ylim([-0.1,1.2])
         plt.ylabel('True Positive Rate')
         plt.xlabel('False Positive Rate')
-        plt.show()
-
-        
-        
+        plt.show()       
         print 'Accuracy:', sklearn.metrics.accuracy_score(y_test, self.my_svm.predict(X_test))
         print 'Prescision:', sklearn.metrics.precision_score(y_test, self.my_svm.predict(X_test))
         print 'Recall:', sklearn.metrics.recall_score(y_test, self.my_svm.predict(X_test))
-        print 'Roc_auc:', self.classification_test(y_test, self.my_svm.predict(X_test))
+        print 'Roc_auc:', roc_auc
 
         
     def predict(self, new_text_file='input.txt'):
